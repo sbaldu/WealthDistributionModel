a93b163580325d4d84ee91884fa00b87d84dcaeb
Grufoony <gregorio_2000@hotmail.it>
2022-11-01 21:39:47 +0100

    added evolve, gamble and couples functions WITHOUT EXCEPTIONS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "./src/network.hpp"
+#include <iostream>
 
 int main() {
-    network net(10,10,10);
-    net.print();
+  network net(10, 10, 10);
+  for (uint8_t i = 0; i < 169; ++i) {
+    net.evolve();
+  }
+  int sum = 0;
+  for (auto i : net.getPlayers()) {
+    sum += i;
+  }
+  net.print();
+  std::cout << sum;
 }
