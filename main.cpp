b2e4403f49dcf09fa2be363c0b03b0cc09bbe0ee
Grufoony <gregorio_2000@hotmail.it>
2022-11-04 23:06:51 +0100

    exp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include "./src/network.hpp"
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 
+void printLoadingBar(int const i, int const n) {
+  std::cout << "Loading: " << std::setprecision(2) << std::fixed
+            << (i * 100. / n) << "%" << '\r';
+  std::cout.flush();
+}
+
 int main() {
-  network net(10, 10, 10);
-  for (uint8_t i = 0; i < 169; ++i) {
+  network net(1, 100, 100);
+  int n = 1e5;
+  for (int i = 0; i < n; ++i) {
+    printLoadingBar(i, n);
     net.evolve();
   }
   int sum = 0;
   for (auto i : net.getPlayers()) {
     sum += i;
   }
-  net.print();
-  std::cout << sum;
+  std::cout << '\n' << '\n';
+  // net.print();
+  net.fprintHist(8);
+  // std::cout << sum;
 }
--- src/network.hpp
+++ src/network.hpp
@@ -4,23 +4,17 @@
 #include <cstdint>
 #include <vector>
 
-struct Couple {
-	uint8_t first;
-	uint8_t second;
-};
-
 class network {
-    private:
-    uint8_t _rows, _cols;
+    uint16_t _rows, _cols;
 	std::vector<uint16_t> _players; // array that contains all the players in the network
 public:
 	network(uint16_t ,uint16_t ,uint16_t);
 	std::vector<uint16_t> const getPlayers();
-	std::vector<Couple> couples(uint8_t); // the couples are chosen
+	uint16_t couples(uint8_t); // the couples are chosen
 	
-	void gamble(Couple); // the players gamble
 	void evolve(); // the couples play
     void print() const noexcept;
+	void fprintHist(uint8_t) const noexcept;
 };
 
 #endif
