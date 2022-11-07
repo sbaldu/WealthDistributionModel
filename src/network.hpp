2e961f099ffa39e980e022e933f493f0167ded3e
Grufoony <gregorio_2000@hotmail.it>
2022-11-07 10:09:45 +0100

    defined struct Player

--- src/network.hpp
+++ src/network.hpp
@@ -4,17 +4,49 @@
 #include <cstdint>
 #include <vector>
 
+struct Player {
+	uint16_t capital;
+	uint8_t nLink;
+
+	Player();
+	Player(uint16_t, uint8_t);
+	Player(const Player& p) : capital(p.capital), nLink(p.nLink) {}
+
+	Player& operator++() {
+		++capital;
+		return *this;
+	}
+	Player& operator--() {
+		--capital;
+		return *this;
+	}
+	Player& operator=(Player const& other) {
+		capital = other.capital;
+		nLink = other.nLink;
+		return *this;
+	}
+	Player& operator+(Player const& other) {
+		capital += other.capital;
+		return *this;
+	}
+	Player& operator-(Player const& other) {
+		capital -= other.capital;
+		return *this;
+	}
+};
+
 class network {
     uint16_t _rows, _cols;
-	std::vector<uint16_t> _players; // array that contains all the players in the network
+	std::vector<Player> _players; // array that contains all the players in the network
 public:
 	network(uint16_t ,uint16_t ,uint16_t);
-	std::vector<uint16_t> const getPlayers();
-	uint16_t couples(uint8_t); // the couples are chosen
+	std::vector<Player> const getPlayers();
+	uint16_t couples(uint16_t); // the couples are chosen
 	
 	void evolveUniform(); // the couples play
 	void evolvePrefAtach(); // the couples play
     void print() const noexcept;
+	void fprintHist() const noexcept;
 	void fprintHist(uint8_t) const noexcept;
 };
 
