e9dcb5add4c70b78df69c1aa72817bc00d793efd
Grufoony <gregorio_2000@hotmail.it>
2022-11-09 23:32:48 +0100

    added BusinessMen functions

--- src/network.h
+++ src/network.h
@@ -41,9 +41,12 @@ class network {
 public:
 	network(uint16_t ,uint16_t ,uint16_t);
 	std::vector<Player> const getPlayers();
+	std::vector<Player> const filterByLinks(uint8_t);
 	std::vector<uint16_t> const playersMoney();
 	std::vector<uint16_t> couples(uint16_t, uint8_t); // the couples are chosen
 	
+	void createBusinessMen(uint8_t, uint8_t);
+	void distributeBusinessMen(uint8_t);
 	void evolveUniform(); // the couples play
 	void evolvePrefAtt(); // the couples play but there is preferential attatchment
 	void flatTax(uint16_t); // the tax is applied to all the players
