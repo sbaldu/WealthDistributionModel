88b7c98d0293c4bd80b9c4bba3d92791dd19d048
Grufoony <gregorio_2000@hotmail.it>
2022-11-12 10:52:49 +0100

    made a class template

--- src/SparseMatrix.hpp
+++ src/SparseMatrix.hpp
@@ -3,26 +3,46 @@
 
 #include <map>
 #include <stdexcept>
+#include <iostream>
 
-class SparseMatrix {
-    std::map<int, double> _matrix;
-    int _rows, _cols;
+template <typename T> class SparseMatrix {
+  std::map<int, T> _matrix;
+  int _rows, _cols;
 
-    public:
-    SparseMatrix(int, int);
-    SparseMatrix(const SparseMatrix&);
+public:
+  SparseMatrix(int rows, int cols) {
+    this->_rows = rows;
+    this->_cols = cols;
+  };
+  SparseMatrix(const SparseMatrix &other) {
+    this->_rows = other._rows;
+    this->_cols = other._cols;
+    this->_matrix = other._matrix;
+  };
 
-    void insert(int , int , double );
-    void erase(int , int );
-    void clear();
-
-    double& operator() (int i, int j) {
-        if(i >= _rows || j >= _cols) {
-            throw std::out_of_range("Index out of range");
-        }
-        return this->_matrix[i*_cols + j];
+  void insert(int i, int j, T value) {
+    if (i >= _rows || j >= _cols) {
+      throw std::out_of_range("Index out of range");
     }
-};
+    _matrix.insert(std::make_pair(i * _cols + j, value));
+  };
+  void erase(int i, int j) { _matrix.erase(i * _cols + j); };
+  void clear() { _matrix.clear(); };
+  void print() {
+    for (int i = 0; i < _rows; ++i) {
+      for (int j = 0; j < _cols; ++j) {
+        std::cout << _matrix[i * _cols + j] << '\t';
+      }
+      std::cout << '\n';
+    }
+  };
 
+  T &operator()(int i, int j) {
+    if (i >= _rows || j >= _cols) {
+      throw std::out_of_range("Index out of range");
+    }
+    return this->_matrix[i * _cols + j];
+  }
+};
 
 #endif
