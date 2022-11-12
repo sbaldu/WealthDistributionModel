1049a5c0332200047798e8717ce990e9fb4e2339
Grufoony <gregorio_2000@hotmail.it>
2022-11-12 19:26:42 +0100

    updated lib

--- src/SparseMatrix.hpp
+++ src/SparseMatrix.hpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <stdexcept>
 #include <iostream>
+#include <fstream>
 
 template <typename T> class SparseMatrix {
   std::map<int, T> _matrix;
@@ -28,6 +29,26 @@ public:
   };
   void erase(int i, int j) { _matrix.erase(i * _cols + j); };
   void clear() { _matrix.clear(); };
+
+  std::map<int, T> getRow(int index) {
+    std::map<int, T> row;
+    for (auto &it : _matrix) {
+      if (it.first / _cols == index) {
+        row.insert(std::make_pair(it.first % _cols, it.second));
+      }
+    }
+    return row;
+  };
+  std::map<int, T> getCol(int index) {
+    std::map<int, T> col;
+    for (auto &it : _matrix) {
+      if (it.first % _cols == index) {
+        col.insert(std::make_pair(it.first / _cols, it.second));
+      }
+    }
+    return col;
+  };
+
   void print() {
     for (int i = 0; i < _rows; ++i) {
       for (int j = 0; j < _cols; ++j) {
@@ -35,6 +56,16 @@ public:
       }
       std::cout << '\n';
     }
+  };
+  void save(const char* fName) {
+    std::ofstream file(fName);
+    for (int i = 0; i < _rows; ++i) {
+      for (int j = 0; j < _cols; ++j) {
+        file << _matrix[i * _cols + j] << '\t';
+      }
+      file << '\n';
+    }
+    file.close();
   };
 
   T &operator()(int i, int j) {
