eba9bf4110c22785c915b113a43a4d7f0be78a73
sbaldu <simone.balducci00@gmail.com>
2022-11-14 19:40:42 +0100

    Added mv to compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,4 @@
 #
 
 g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) ./src/binding.cc -o network$(python3-config --extension-suffix)
+mv *.so ~/.local/lib/python*/site-packages/
