2490cb8241bf978d8effaafd285e9aec1285cf33
SimonB00 <simone.balducci@gmail.com>
2022-11-18 19:20:15 +0100

    Testing in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import network
 
 def data(vec):
@@ -9,48 +10,44 @@ def data(vec):
     for element in vec:
         y[element] += 1
     return x,y
+def fitfunc(x,a,A):
+    return A/x**a
 
-net = network.network(1,100,100)
+net = network.network(5,100,100)
 n = 10**5
 for i in range(n):
     net.evolveUniform()
-
 #hist = plt.hist(net.playersMoney())
 plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
 plt.yscale("log")
+plt.title("10^5 iterations")
+# plt.savefig("./plots/105.png")
 plt.show()
-plt.savefig("prova_0.png")
-plt.clear()
+# plt.clear()
 
-net = network.network(1,100,100)
+net = network.network(5,100,100)
 n = 10**6
 for i in range(n):
     net.evolveUniform()
-
 #hist = plt.hist(net.playersMoney())
 plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
 plt.yscale("log")
+plt.title("10^6 iterations")
+# plt.savefig("./plots/106.png")
 plt.show()
-plt.savefig("prova_1.png")
-plt.clear()
+# plt.clear()
 
-net = network.network(1,100,100)
+net = network.network(5,100,100)
 n = 10**7
 for i in range(n):
     net.evolveUniform()
-
 #hist = plt.hist(net.playersMoney())
 plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
 plt.yscale("log")
+plt.title("10^7 iterations")
+# plt.savefig("./plots/107.png")
 plt.show()
-plt.savefig("prova_2.png")
-plt.clear()
+# plt.clear()
 
 net = network.network(1,100,100)
 n = 10**6
@@ -58,61 +55,65 @@ for i in range(n):
     net.evolvePrefAtt()
     if i % 1000 == 0:
         net.flatTax(10)
-
 #hist = plt.hist(net.playersMoney())
 plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
 plt.yscale("log")
 plt.xscale("log")
+plt.title("10% flat tax every 1000 iterations")
+# plt.savefig("./plots/1000-10.png")
 plt.show()
-plt.savefig("prova_3.png")
-plt.clear()
 
 net = network.network(1,100,100)
 for i in range(n):
     net.evolvePrefAtt()
     if i % 1500 == 0:
         net.flatTax(10)
-
 #hist = plt.hist(net.playersMoney())
-plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
+x = data(net.playersMoney())[0][1::]
+y = data(net.playersMoney())[1][1::]
+# popt, _ = curve_fit(fitfunc,x,y)
+# a, A = popt
+plt.plot(x,y,'bo')
+# plt.plot(x,[fitfunc(x_,a,A) for x_ in x])
+plt.plot(x,[2000/x_**2 for x_ in x])
 plt.yscale("log")
 plt.xscale("log")
+plt.title("10% flat tax every 1500 iterations")
+# plt.savefig("./plots/1500-10.png")
 plt.show()
-plt.savefig("prova_4.png")
-plt.clear()
 
 net = network.network(1,100,100)
 for i in range(n):
     net.evolvePrefAtt()
     if i % 2000 == 0:
         net.flatTax(10)
-
 #hist = plt.hist(net.playersMoney())
 plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
 plt.yscale("log")
 plt.xscale("log")
+plt.title("10% flat tax every 2000 iterations")
+# plt.savefig("./plots/2000-10.png")
 plt.show()
-plt.savefig("prova_5.png")
-plt.clear()
 
 net = network.network(1,100,100)
 for i in range(n):
     net.evolvePrefAtt()
     if i % 500 == 0:
         net.flatTax(10)
-
 #hist = plt.hist(net.playersMoney())
 plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-
-# plt.show(hist,best_fit_line)
 plt.yscale("log")
 plt.xscale("log")
+plt.title("10% flat tax every 500 iterations")
+# plt.savefig("./plots/500-10.png")
 plt.show()
-plt.savefig("prova_6.png")
-plt.clear()
+
+# net = network.network(1,100,100)
+# net.distributeBusinessMen(100)
+# for i in range(n):
+#     net.evolveUniform()
+# plt.hist(net.filterByLinks(2))
+# plt.title("Multiple links")
+# # plt.yscale("log")
+# # plt.xscale("log")
+# plt.show()
