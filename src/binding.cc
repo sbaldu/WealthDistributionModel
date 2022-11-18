6f1408cfc156b090c4a8381d07ffedd297f2c701
SimonB00 <simone.balducci@gmail.com>
2022-11-18 19:19:30 +0100

    Creating and binding new network

--- src/binding.cc
+++ src/binding.cc
@@ -1,5 +1,7 @@
+#include "SparseMatrix.hpp"
 #include "network.cc"
 #include "network.h"
+#include <cstdint>
 #include <functional>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -9,11 +11,37 @@
 PYBIND11_MODULE(network, m) {
   m.doc() = "Boltzmann";
 
+  pybind11::class_<SparseMatrix<bool>>(m, "SparseMatrix")
+      .def(pybind11::init<>())
+      .def(pybind11::init<int, int>())
+      .def(pybind11::init<SparseMatrix<bool>>())
+      .def("insert", static_cast<void (SparseMatrix<bool>::*)(int, int, bool)>(
+                         &SparseMatrix<bool>::insert))
+      .def("insert", static_cast<void (SparseMatrix<bool>::*)(int, bool)>(
+                         &SparseMatrix<bool>::insert))
+      .def("insert_or_assign", &SparseMatrix<bool>::insert_or_assign)
+      .def("erase", &SparseMatrix<bool>::erase)
+      .def("clear", &SparseMatrix<bool>::clear)
+      .def("exists", &SparseMatrix<bool>::exists)
+      .def("getRow", &SparseMatrix<bool>::getRow)
+      .def("getCol", &SparseMatrix<bool>::getCol)
+      .def("getRowDim", &SparseMatrix<bool>::getRowDim)
+      .def("getColDim", &SparseMatrix<bool>::getColDim)
+      .def("size", &SparseMatrix<bool>::size)
+      .def("print", &SparseMatrix<bool>::print)
+      .def("save", &SparseMatrix<bool>::save);
+
   pybind11::class_<network>(m, "network")
       .def(pybind11::init<uint16_t, uint16_t, uint16_t>())
       .def("getPlayers", &network::getPlayers)
+      .def("getAdjacency", &network::getAdjacency)
       .def("couples", &network::couples)
+		.def("playersMoney", static_cast<std::vector<uint16_t> const (network::*)()>(&network::playersMoney)
+		.def("playersMoney", static_cast<std::vector<uint16_t> const (network::*)(std::vector<uint16_t> const&)>(&network::playersMoney))
+      .def("createLinks", &network::createLinks)
+      .def("exists", &network::exists)
       .def("evolveUniform", &network::evolveUniform)
       .def("evolvePrefAtt", &network::evolvePrefAtt)
+      .def("flatTax", &network::flatTax)
       .def("print", &network::print);
 }
