53bff911a99d5ec9b4002a621ee1045736c3f790
SimonB00 <simone.balducci@gmail.com>
2022-11-28 12:52:17 +0100

    Restored getAdjacency. Implemented tracking of the poor.

--- testLinks.py
+++ testLinks.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import network as n
 
-net = n.network(1,50,50)
+net = n.network(5,50,50)
 adjacency = net.getAdjacency()
 net.createLinks(4)
 
@@ -10,6 +10,9 @@ n = 10**5
 for i in range(n):
     matrix_el = np.random.choice(list(net.getAdjacency().keys()))
     net.evolveFixed(matrix_el)
+print(net.getPoors())
 
+plt.hist(net.getPoors(),bins=500)
+plt.show()
 plt.hist(net.getPlayers())
 plt.show()
