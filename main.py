eb9d58a9a48a58e7f88b5c26b59ad49b30c0e9e7
Grufoony <gregorio_2000@hotmail.it>
2022-11-28 16:42:49 +0100

    pyROOT implementatio

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 import network
 import ROOT
 from ROOT import gStyle
+import matplotlib
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
 
 def data(vec):
     x = np.arange(0, max(vec)+1)
@@ -16,6 +25,40 @@ def data(vec):
 # FAIR GAME
 ####################################################################################################
 
+net = network.network(5,100,100)
+n = 10**6
+for i in range(n):
+    net.evolveUniform()
+
+x = data(net.playersMoney())[0][1::].tolist()
+y = data(net.playersMoney())[1][1::].tolist()
+y = [i/sum(y) for i in y] # normalization
+
+gStyle.SetOptFit(1111)
+
+h = ROOT.TH1F("h", "10^6 iterations", 60, 0, 60)
+for i in range(len(x)):
+    h.Fill(x[i], y[i])
+
+# fitting and extracting fit function
+h.Fit("expo")
+f = h.GetListOfFunctions().FindObject("expo")
+
+plt.plot(x, y ,'bo')
+fitX = np.arange(min(x), max(x), 0.1)
+fitY = abs(f.GetParameter("Slope"))*np.exp(f.GetParameter("Slope")*fitX)
+plt.plot(fitX, fitY)
+plt.yscale("log")
+plt.title("10^{6} iterations")
+plt.savefig("prova.pgf")
+
+# canv = ROOT.TCanvas("canv", "canv", 800, 600)
+# canv.SetLogy()
+
+# h.Draw()
+
+
+
 # net = network.network(5,100,100)
 # n = 10**5
 # for i in range(n):
@@ -26,27 +69,6 @@ def data(vec):
 # # plt.savefig("./plots/105.png")
 # plt.show()
 # # plt.clear()
-
-net = network.network(5,100,100)
-n = 10**6
-for i in range(n):
-    net.evolveUniform()
-#hist = plt.hist(net.playersMoney())
-x = data(net.playersMoney())[0][1::].tolist()
-y = data(net.playersMoney())[1][1::].tolist()
-
-gStyle.SetOptFit(1111)
-
-h = ROOT.TH1F("h", "10^6 iterations", 60, 0, 60)
-for i in range(len(x)):
-    h.Fill(x[i], y[i])
-# f = ROOT.TF1("f", "[0]*e^(-[1]*x)")
-h.Fit("expo")
-
-canv = ROOT.TCanvas("canv", "canv", 800, 600)
-canv.SetLogy()
-
-h.Draw()
 
 # net = network.network(5,100,100)
 # n = 10**7
@@ -118,12 +140,12 @@ h.Draw()
 # # UNFAIR GAME WITHOUT REDISTRIBUTION THROUGH FLAT TAX
 # ####################################################################################################
 
-net = network.network(1,100,100)
-for i in range(n):
-    net.evolvePrefAttNoTax()
-    if i % 1500:
-        net.flatTax(10)
-plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
-plt.yscale("log")
-plt.xscale("log")
-plt.show()
+# net = network.network(1,100,100)
+# for i in range(n):
+#     net.evolvePrefAttNoTax()
+#     if i % 1500:
+#         net.flatTax(10)
+# plt.plot(data(net.playersMoney())[0],data(net.playersMoney())[1],'bo')
+# plt.yscale("log")
+# plt.xscale("log")
+# plt.show()
