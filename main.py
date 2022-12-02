1fb2496f96ab0d8162bab093271aeadac1864d4d
Grufoony <gregorio_2000@hotmail.it>
2022-12-02 16:17:24 +0100

    test

--- main.py
+++ main.py
@@ -191,35 +191,54 @@ def data(vec):
 ###################################################################################
 ### MODEL WITH SAVINGS
 ###################################################################################
-net = network.network(50,100,100)
-n = 15*10**4
+# net = network.network(50,100,100)
+# n = 15*10**4
+# for i in tqdm(range(n)):
+#     net.evolveSavings()
+
+# x = data(net.playersMoney())[0][1::].tolist()
+# fitX = np.arange(min(x), max(x), 0.1)
+# y = data(net.playersMoney())[1][1::].tolist()
+# y = [i/sum(y) for i in y] # normalization
+
+# h = ROOT.TH1F("h", "Fair Game", len(x), min(x), max(x))
+# for i in range(len(x)):
+#     h.Fill(x[i], y[i])
+
+# # fitting and extracting fit function
+# h.Fit("expo")
+
+# f = h.GetListOfFunctions().FindObject("expo")
+# fitY = np.exp(f.GetParameter("Constant")+f.GetParameter("Slope")*fitX)
+
+# g = ROOT.TF1("g", "[0]*x^[1]", 40, max(x))
+# h.Fit("g", "R")
+
+# # plt.plot(x, y ,'bo')
+# plt.hist(net.getPlayers(), density=True)
+# plt.plot(fitX, fitY, color="red")
+# fitX = np.arange(40, max(x), 0.1)
+# fitY = g.GetParameter(0)*(fitX**g.GetParameter(1))
+# plt.plot(fitX, fitY, color="green")
+# plt.yscale("log")
+# # plt.xscale("log")
+# plt.show()
+
+###################################################################################
+### MODEL WITH FIXED NETWORK
+###################################################################################
+
+net = network.network(1,40,40)
+adjacency = net.getAdjacency()
+net.createLinks(9)
+
+n = 10**4
 for i in tqdm(range(n)):
-    net.evolveSavings()
+    matrix_el = np.random.choice(list(net.getAdjacency().keys()))
+    net.evolveFixed(matrix_el)
+print(net.getPoors())
 
-x = data(net.playersMoney())[0][1::].tolist()
-fitX = np.arange(min(x), max(x), 0.1)
-y = data(net.playersMoney())[1][1::].tolist()
-y = [i/sum(y) for i in y] # normalization
-
-h = ROOT.TH1F("h", "Fair Game", len(x), min(x), max(x))
-for i in range(len(x)):
-    h.Fill(x[i], y[i])
-
-# fitting and extracting fit function
-h.Fit("expo")
-
-f = h.GetListOfFunctions().FindObject("expo")
-fitY = np.exp(f.GetParameter("Constant")+f.GetParameter("Slope")*fitX)
-
-g = ROOT.TF1("g", "[0]*x^[1]", 40, max(x))
-h.Fit("g", "R")
-
-# plt.plot(x, y ,'bo')
-plt.hist(net.getPlayers(), density=True)
-plt.plot(fitX, fitY, color="red")
-fitX = np.arange(40, max(x), 0.1)
-fitY = g.GetParameter(0)*(fitX**g.GetParameter(1))
-plt.plot(fitX, fitY, color="green")
-plt.yscale("log")
-# plt.xscale("log")
+plt.hist(net.getPoors(),bins=50)
+plt.show()
+plt.hist(net.getPlayers())
 plt.show()
