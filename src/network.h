e97fecd37c873d5e0960aba91ab63ca961d4e302
Grufoony <gregorio_2000@hotmail.it>
2022-12-06 17:07:43 +0100

    renewed link dynamics

--- src/network.h
+++ src/network.h
@@ -11,19 +11,16 @@
 using Matrix = SparseMatrix<bool>;
 
 class network {
-	uint16_t rows_, cols_;
-	std::vector<uint16_t> players_; // array that contains all the players in the network
-	std::unordered_map<uint16_t,int>	poorMap_;
+	uint16_t rows_, cols_, cass_ = 0, initCap_;
+	std::vector<uint16_t> players_, poors_; // array that contains all the players in the network
 	Matrix adjacencyMatrix_;
 	std::vector<int> nLinks_;
-	uint16_t cass_ = 0;
 public:
 	network(uint16_t ,uint16_t ,uint16_t);
 	uint16_t const getRows();
 	uint16_t const getCols();
 	std::vector<uint16_t> const& getPlayers();
-	std::unordered_map<uint16_t,int> const& getPoors();
-	std::unordered_map<int, bool> const& getAdjacency();
+	std::vector<uint16_t> const& getPoors();
 	std::vector<uint16_t> const playersMoney();
 	/* std::vector<uint16_t> const playersMoney(std::vector<uint16_t> const&); */
 	uint16_t couples(uint16_t); // the couples are chosen
@@ -31,15 +28,16 @@ public:
 	void createLinks(uint8_t);
 	void importMatrix(const char*);
 	bool exists(int, int);
+	bool isPoor(uint16_t);
 	void printMatrix();
 	void evolveUniform(); // the couples play
 	void evolvePrefAtt();
 	void evolvePrefAttNoTax();
-	void evolveFixed(int);
+	void evolveFixed();
 	void flatTax(uint8_t);
 	void evolveSavings();
 	float checkPoor(uint16_t);
-	float calcCondProb(std::unordered_map<uint16_t, int>);
+	float calcCondProb(std::vector<uint16_t> const&);
 	void print() const noexcept;
 	void fprintHist() const noexcept;
 	void fprintHist(uint8_t) const noexcept;
