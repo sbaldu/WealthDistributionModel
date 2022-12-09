a07ad7c39dcd34d595af97842bc436b2031d6ec1
sbaldu <simone.balducci00@gmail.com>
2022-12-09 15:03:06 +0100

    First draft for the analysis of data

--- analysis.py
+++ analysis.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("./data/adult_cleaned.csv")
 data = df['fnlwgt'].values.tolist()
 
-nBins = 500
+nBins = 200
 plt.hist(data, bins=nBins)
 plt.xlabel('Capital (US dollars)')
 plt.ylabel('Frequency')
-# plt.savefig("./tex/img/global_real_distribution.pgf")
-plt.show()
+plt.savefig("./tex/img/global_real_distribution.pgf")
+plt.yscale('log')
+plt.savefig("./tex/img/real_head.pgf")
+plt.xscale('log')
+plt.savefig("./tex/img/real_tail.pgf")
+# plt.show()
