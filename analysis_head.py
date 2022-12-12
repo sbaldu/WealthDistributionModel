59fdc2f71982e835590d541e1b260e2445f7f3b2
sbaldu <simone.balducci00@gmail.com>
2022-12-12 17:33:41 +0100

    Fit in head of real data

--- analysis_head.py
+++ analysis_head.py
@@ -19,11 +19,14 @@ gStyle.SetOptFit(1111)
 df = pd.read_csv("./data/adult_cleaned.csv")
 data = df['fnlwgt'].values.tolist()
 
-nBins = 500
+nBins = 200
 
 min_head = 2*10**5
-max_head = 3*10**5
-h2 = ROOT.TH1F("h2", "Fair Game", max_head-min_head, min_head, max_head)
+max_head = 6*10**5
+data = [x for x in data if x >= min_head and x <= max_head]
+n,bins,patches = plt.hist(data, range=(min_head,max_head))
+
+h2 = ROOT.TH1F("h2", "Fair Game", nBins, min_head, max_head)
 for i in range(len(data)):
     h2.Fill(data[i])
 
@@ -35,7 +38,12 @@ print("p-value = " + str(round(f.GetProb(), 3)))
 
 fitX = np.arange(min_head, max_head, 0.1)
 fitY = np.exp(f.GetParameter("Constant")+f.GetParameter("Slope")*fitX)
-plt.hist(data, range=(min_head,max_head))
+
+# For rinormalization reasons we shift the fit line upward
+shift = np.log(n[0]) - np.log(fitY[0]) 
+shift_ampl = np.exp(shift)
+fitY = [shift_ampl*y for y in fitY]
+
 plt.plot(fitX, fitY, color="red")
 plt.yscale("log")
 plt.xlabel('Capital (US dollars)')
