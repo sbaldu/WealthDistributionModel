9d669aed2af237059525ee797d932e5629c6559f
SimonB00 <simone.balducci@gmail.com>
2022-12-13 12:41:49 +0100

    Plotted dist of real data in log-log

--- analysis.py
+++ analysis.py
@@ -1,16 +1,22 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("./data/adult_cleaned.csv")
 data = df['fnlwgt'].values.tolist()
 
 nBins = 200
-n, Bins, patches = plt.hist(data, bins=nBins,density=True)
+n, Bins, patches = plt.hist(data, bins=nBins, density=True)
 plt.xlabel('Capital (US dollars)')
 plt.ylabel('Frequency')
 plt.savefig("./tex/img/global_real_distribution.pgf")
 plt.yscale('log')
 plt.savefig("./tex/img/real_head.pgf")
+plt.clf()
+sns.distplot(data, hist=False)
+plt.xlabel('Capital (US dollars)')
+plt.ylabel('Frequency')
+plt.yscale('log')
 plt.xscale('log')
 plt.savefig("./tex/img/real_tail.pgf")
 # plt.show()
