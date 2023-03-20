92d13d2e7cd2e20e00bd7aaa018c47c9adf9e5c7
sbaldu <simone.balducci00@gmail.com>
2023-03-20 15:23:01 +0100

    Removed fsanitize flag in compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,4 @@
 #
 
-g++ -O3 -Wall -shared -fsanitize=leak -std=c++20 -fPIC $(python3 -m pybind11 --includes) ./src/binding.cc -o network$(python3-config --extension-suffix)
+g++ -O3 -Wall -shared -std=c++20 -fPIC $(python3 -m pybind11 --includes) ./src/binding.cc -o network$(python3-config --extension-suffix)
 mv *.so ~/.local/lib/python*/site-packages/
