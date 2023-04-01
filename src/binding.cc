a41ef910f9800b47f67a65a6250eb960382c9147
sbaldu <simone.balducci00@gmail.com>
2023-04-01 21:02:53 +0200

    Removed binding of SparseMatrix

--- src/binding.cc
+++ src/binding.cc
@@ -11,40 +11,13 @@
 PYBIND11_MODULE(network, m) {
   m.doc() = "Boltzmann";
 
-  pybind11::class_<SparseMatrix<bool>>(m, "SparseMatrix")
-      .def(pybind11::init<>())
-      .def(pybind11::init<int, int>())
-      .def(pybind11::init<int>())
-      .def(pybind11::init<SparseMatrix<bool> const &>())
-      .def("insert", static_cast<void (SparseMatrix<bool>::*)(int, int, bool)>(&SparseMatrix<bool>::insert))
-      .def("insert", static_cast<void (SparseMatrix<bool>::*)(int, bool)>(&SparseMatrix<bool>::insert))
-      .def("insert_or_assign",
-           static_cast<void (SparseMatrix<bool>::*)(int, int, bool)>(&SparseMatrix<bool>::insert_or_assign))
-      .def("insert_or_assign",
-           static_cast<void (SparseMatrix<bool>::*)(int, bool)>(&SparseMatrix<bool>::insert_or_assign))
-      .def("erase", &SparseMatrix<bool>::erase)
-      .def("clear", &SparseMatrix<bool>::clear)
-      .def("contains", static_cast<bool (SparseMatrix<bool>::*)(int, int) const>(&SparseMatrix<bool>::contains))
-      .def("contains", static_cast<bool (SparseMatrix<bool>::*)(int) const>(&SparseMatrix<bool>::contains))
-      .def("getRow", &SparseMatrix<bool>::getRow)
-      .def("getCol", &SparseMatrix<bool>::getCol)
-      .def("getRndRowElement", &SparseMatrix<bool>::getRndRowElement)
-      .def("getRndColElement", &SparseMatrix<bool>::getRndColElement)
-      .def("getNormRows", &SparseMatrix<bool>::getNormRows)
-      .def("getNormCols", &SparseMatrix<bool>::getNormCols)
-      .def("getRowDim", &SparseMatrix<bool>::getRowDim)
-      .def("getColDim", &SparseMatrix<bool>::getColDim)
-      .def("size", &SparseMatrix<bool>::size)
-      .def("print", &SparseMatrix<bool>::print);
-
   pybind11::class_<network>(m, "network")
       .def(pybind11::init<uint16_t, uint16_t, uint16_t>())
       .def("getRows", &network::getRows)
       .def("getCols", &network::getCols)
-      .def("getPlayers", &network::getPlayers)
+      .def("players", &network::players)
       .def("getPoors", &network::getPoors)
       .def("couples", &network::couples)
-      .def("playersMoney", &network::playersMoney)
       .def("createLinks", &network::createLinks)
       .def("exists", &network::exists)
       .def("printMatrix", &network::printMatrix)
@@ -54,7 +27,6 @@ PYBIND11_MODULE(network, m) {
       .def("evolveFixed", &network::evolveFixed)
       .def("evolvebyLink", &network::evolvebyLink)
       .def("flatTax", &network::flatTax)
-      .def("evolveSavings", &network::evolveSavings)
       .def("checkPoor", &network::checkPoor)
       .def("calcProb", &network::calcProb)
       .def("print", &network::print);
